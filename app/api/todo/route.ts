adb2d62eb5c277e36db4acf98effb5ee4cafa052
LsDevFrance <liron.devlopper@gmail.com>
2025-05-22 19:51:24 +0200

    first commit

---
+++ app/api/todo/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  return NextResponse.json({ message: "Hello World" });
+};
+
+export const POST = async () => {
+  return NextResponse.json({ message: "Hello World" });
+};
+
+export const PUT = async () => {
+  return NextResponse.json({ message: "Hello World" });
+};
+
+export const DELETE = async () => {
+  return NextResponse.json({ message: "Hello World" });
+};
